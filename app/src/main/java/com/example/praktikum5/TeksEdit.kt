0aeb86e0cb1fc83d2db7de4b388983d040b70d9e
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-10-30 08:56:19 +0700

    variabel untuk mengingat nilai masukan dari keyboard

--- app/src/main/java/com/example/praktikum5/TeksEdit.kt
+++ app/src/main/java/com/example/praktikum5/TeksEdit.kt
@@ -1,8 +1,17 @@
 package com.example.praktikum5
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import java.lang.reflect.Modifier
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 @Composable
-fun  FormDataDiri(){
+fun  FormDataDiri(modifier: Modifier){
+    //variabel untuk mengingat nilai masukan dari keyboard
+    var textNama by remember { mutableStateOf(value = "") }
+    var textAlamat by remember { mutableStateOf(value = "") }
+    var textJK by remember { mutableStateOf(value = "") }
 
 }
