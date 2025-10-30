73ecff976b2e2492ae609062bf86895864739100
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-10-30 08:58:03 +0700

    variabel untuk menyimpan dta yang diperoleh

--- app/src/main/java/com/example/praktikum5/TeksEdit.kt
+++ app/src/main/java/com/example/praktikum5/TeksEdit.kt
@@ -14,4 +14,9 @@ fun  FormDataDiri(modifier: Modifier){
     var textAlamat by remember { mutableStateOf(value = "") }
     var textJK by remember { mutableStateOf(value = "") }
 
+    //variabel untuk menyimpan dta yang diperoleh
+    var nama by remember { mutableStateOf(value = "") }
+    var alamat by remember { mutableStateOf(value = "") }
+    var jenis by remember { mutableStateOf(value = "") }
+
 }
