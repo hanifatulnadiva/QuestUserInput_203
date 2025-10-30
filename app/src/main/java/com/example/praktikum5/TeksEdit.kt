920c6bc6242dad3e0cf5a3f32bb63759de5a8331
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-10-30 13:17:26 +0700

    add elevation untuk mengatur bayangan pada card

--- app/src/main/java/com/example/praktikum5/TeksEdit.kt
+++ app/src/main/java/com/example/praktikum5/TeksEdit.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.Button
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
@@ -106,7 +107,9 @@ fun  FormDataDiri(modifier: Modifier){
             thickness = dimensionResource(id=R.dimen.divider_tipis),
             color = Color.DarkGray
         )
-        ElevatedCard {  }
+        ElevatedCard(
+            elevation = CardDefaults.cardElevation(defaultElevation = 10.dp)
+        ) {  }
     }
 
 }
