268e2cb7e1e2dbc65d682f69298684aefea33c58
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-11-01 00:11:02 +0700

    add variabel untuk mengingat nilai masukan dari keyboard

--- app/src/main/java/com/example/formulir/ui/theme/FormulirPendaftaran.kt
+++ app/src/main/java/com/example/formulir/ui/theme/FormulirPendaftaran.kt
@@ -1,9 +1,18 @@
 package com.example.formulir.ui.theme
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 @Composable
 fun FormulirPendaftaran(modifier: Modifier){
+    var textNama by remember { mutableStateOf(value="") }
+    var textJK by remember { mutableStateOf(value="") }
+    var textStatus by remember { mutableStateOf(value="") }
+    var textAlamat by remember { mutableStateOf(value="") }
 
 }
+
