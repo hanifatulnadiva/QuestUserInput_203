0fe6ac14c541329f7d73f12ed2a2597c71acc8f1
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-10-31 23:59:39 +0700

    membuat file baru lalu membuat fungsi FormulirPendataran dengan parameter modifier

---
+++ app/src/main/java/com/example/formulir/ui/theme/FormulirPendaftaran.kt
@@ -0,0 +1,9 @@
+package com.example.formulir.ui.theme
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FormulirPendaftaran(modifier: Modifier){
+
+}
