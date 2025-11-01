dcce0818652821097ad75d4002fcc7a08fb1a53f
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-11-01 07:00:47 +0700

    add layou Row lalu menggatur jarak dari atas 40dp, tinggi layoutnya 100dp,warna backgrounnya menjadi ungu, dan mengatur komponennya supaya ada di tengah

--- app/src/main/java/com/example/formulir/FormulirPendaftaran.kt
+++ app/src/main/java/com/example/formulir/FormulirPendaftaran.kt
@@ -1,16 +1,25 @@
-package com.example.formulir.ui.theme
+package com.example.formulir
 
-import android.R
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun FormulirPendaftaran(modifier: Modifier){
@@ -29,7 +38,20 @@ fun FormulirPendaftaran(modifier: Modifier){
 
     Column  (modifier= Modifier
         .fillMaxSize()
-        .background(color=Color.White)) {  }
+        .background(color=Color.White)
+    ){
+        Row (modifier= Modifier
+            .padding(top = 40.dp)
+            .fillMaxWidth()
+            .height(height = 100.dp)
+            .background(color = colorResource(id = R.color.purple_200)),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+            ){
+
+        }
+
+    }
 
 }
 
